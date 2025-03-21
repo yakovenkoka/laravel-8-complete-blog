2d177007ab96c8d3e74069c9fd9faac5bd80a8e4
yakovenkoka <D00272517@student.dkit.ie>
2025-03-21 00:58:51 +0000

    small fix

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="w-4/5 m-auto text-center">
     <div class="py-15 border-b border-gray-200">
-        <h1 class="text-6xl">
+        <h1 class="text-5xl font-bold text-black">
             Blog Posts
         </h1>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
                     <a class="no-underline text-lg font-semibold hover:text-orange-400 hover:scale-105 transition-transform duration-300 {{ request()->is('/') ? 'text-orange-400' : '' }}" href="{{ url('/') }}">Home</a>
                     <a class="no-underline text-lg font-semibold hover:text-orange-400 hover:scale-105 transition-transform duration-300 {{ request()->is('blog') ? 'text-orange-400' : '' }}" href="{{ url('/blog') }}">Blog</a>
                     <a class="no-underline text-lg font-semibold hover:text-orange-400 hover:scale-105 transition-transform duration-300 {{ request()->is('about') ? 'text-orange-400' : '' }}" href="{{ url('/about') }}">About Us</a>
+                    <a class="no-underline text-lg font-semibold hover:text-orange-400 hover:scale-105 transition-transform duration-300 {{ request()->is('contact') ? 'text-orange-400' : '' }}" href="{{ url('/contact') }}">Contact Us</a>
                     @guest
                         @if (Route::has('login'))
                             <a class="no-underline text-lg font-semibold hover:text-orange-400 hover:scale-105 transition-transform duration-300 {{ request()->is('login') ? 'text-orange-400' : '' }}" href="{{ route('login') }}">{{ __('Login') }}</a>
